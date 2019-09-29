d417095abfa7b68d943384484d99f945375b9d52
Sayter99 <ing5698yi@hotmail.com>
2019-09-29 10:29:47 -0600

    implement boxplot api for parallel array

--- data_viz.py
+++ data_viz.py
@@ -1,13 +1,22 @@
 import matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')
 
 
-def boxplot(par, meta, x_label, y_label, title, out_file):
-    """plot boxplot for an input array and save the result as a png file
+def boxplot(data, meta, x_label, y_label, title, out_file):
+    """plot boxplot for input parallel array and save the result as a png file
     """
-    plt.boxplot(par)
-    plt.ylabel('Distribution')
-    plt.xlabel('Box')
-    plt.savefig(out_file)
-    pass
+    fig, ax1 = plt.subplots(figsize=(12, 8))
+
+    ax1.boxplot(data)
+
+    # Hide these grid behind plot objects
+    ax1.set_axisbelow(True)
+    ax1.set_title(title)
+    ax1.set_xlabel(x_label)
+    ax1.set_ylabel(y_label)
+
+    # set tick labels and export the result
+    ax1.set_xticklabels(meta, rotation=90, fontsize=8)
+    fig.savefig(out_file, bbox_inches='tight')
